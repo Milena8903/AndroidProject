46bacb9f0418da6349bd5d3182a78f1f6ea86de8
Milena <milena_8903@hotmail.com>
2021-04-14 21:46:13 -0500

    First upload

---
+++ app/src/main/java/edu/ue/appmovil/nda/appfinanzas/MainActivity.java
@@ -0,0 +1,21 @@
+package edu.ue.appmovil.nda.appfinanzas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    protected void updateCalendar(Bundle saveCalendar){
+        // implement later my nigga
+        //super.onActivityReenter(saveCalendar);
+        //setContentView(R.Lay);
+    }
+}
